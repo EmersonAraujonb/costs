0b364d0090f115fb5fa45475eeb7da43a8f7233b
Emerson Araujo <emersonsantos@gmail.com>
2023-05-12 13:36:50 -0300

    feat: update footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,9 +6,8 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         <ul className={styles.social_list}>
-            <li><FaFacebook/></li>
-            <li><FaInstagram/></li>
-            <li><FaLinkedin/></li>
+            <li><a href="https://www.instagram.com/the_developerfrontend/"><FaInstagram/></a></li>
+            <li><a href="https://www.linkedin.com/in/dev-emerson-araujo/"><FaLinkedin/></a></li>
         </ul>
         <p className={styles.copy_right}><span>Costs</span> &copy; 2023</p>
     </footer>
